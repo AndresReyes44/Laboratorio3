9eadf9e99fd114e4d0491167ea4cf611592fc8b9
AndresReyes44 <A01562038@items.mx>
2021-03-20 22:40:30 -0600

    first commit

---
+++ src/main/java/mx/tec/lab/DothrakiTranslator.java
@@ -0,0 +1,21 @@
+package mx.tec.lab;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class DothrakiTranslator {
+	
+	Map<String, String> translationMap;
+
+	public DothrakiTranslator() {
+		translationMap = new HashMap<>();
+	}
+
+	public void addTranslation(final String word, final String translation) {
+		this.translationMap.put(word, translation);
+	}
+
+	public String getTranslation(final String word) {
+		return this.translationMap.get(word);
+	}
+}
